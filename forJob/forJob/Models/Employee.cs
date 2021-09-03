9d6c51e0bf6669456a73301dee5b9044a4c8a001
Larry <larrylinr5@gmail.com>
2021-09-03 20:50:17 +0800

    新增 Employee models

---
+++ forJob/forJob/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace forJob.Models
+{
+    public class Employee
+    {
+        [Display(Name = "員工編號")]
+        public int Id { get; set; }
+        [Display(Name = "姓名")]
+        public string Name { get; set; }
+        [Display(Name = "行動電話")]
+        public string Mobile { get; set; }
+        [Display(Name = "電子郵件")]
+        public string Email { get; set; }
+        [Display(Name = "部門")]
+        public string Department { get; set; }
+        [Display(Name = "職稱")]
+        public string Title { get; set; }
+    }
+}
